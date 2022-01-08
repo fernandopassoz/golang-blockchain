1855c056b20a4d173c01ad03abd65b1c0903b5e9
Fernando Passos <fernandopassoz@gmail.com>
2022-01-08 12:43:35 -0300

    runctions refactored

--- main.go
+++ main.go
@@ -1,31 +1,24 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
-	"encoding/hex"
+	"encoding/binary"
 	"errors"
 	"fmt"
-	"strconv"
 )
 
-// type Block struct {
-// 	Index        uint64
-// 	Nonce        int32
-// 	Data         []byte
-// 	PreviousHash []byte
-// 	Hash         []byte
-// }
 type Block struct {
 	Index        uint64
 	Nonce        int32
-	Data         string
-	PreviousHash string
-	Hash         string
+	Data         []byte
+	PreviousHash []byte
+	Hash         []byte
 }
 
 type Chain struct {
 	Dificulty int
-	LastHash  string
+	LastHash  []byte
 	Blocks    []*Block
 }
 
@@ -34,56 +27,87 @@ func (chain *Chain) WriteBlock(block *Block) error {
 	return errors.New("error")
 }
 
-func (chain *Chain) PoW(block *Block) error {
-	golden := false
-	nonce := 0
-	sum := sha256.Sum256([]byte(block.Data + block.PreviousHash + strconv.FormatUint(uint64(block.Index), 10) + strconv.Itoa(nonce)))
-	var hash string = hex.EncodeToString(sum[:])
+// func (chain *Chain) PoW(block *Block) (error, bool){
+// 	golden := false
+// 	nonce := 0
+// 	sum := sha256.Sum256([]byte(block.Data + block.PreviousHash + strconv.FormatUint(uint64(block.Index), 10) + strconv.Itoa(nonce)))
+// 	var hash string = hex.EncodeToString(sum[:])
 
-	for !golden {
-		// if string(block.PreviousHash)[:chain.Dificulty] == string(hash)[:chain.Dificulty] {
-		if "00" == string(hash)[:chain.Dificulty] {
-			golden = true
-			block.Nonce = int32(nonce)
-			block.Hash = string(hash)
-			chain.WriteBlock(block)
-		} else {
-			nonce = nonce + 1
-			fmt.Println(nonce, hash)
-			// fmt.Println(block.Data + block.PreviousHash + strconv.FormatUint(uint64(block.Index), 10) + strconv.Itoa(nonce))
-		}
+// 	for !golden {
+// 		// if string(block.PreviousHash)[:chain.Dificulty] == string(hash)[:chain.Dificulty] {
+// 		if "00" == string(hash)[:chain.Dificulty] {
+// 			golden = true
+// 			block.Nonce = int32(nonce)
+// 			block.Hash = hash
+// 			chain.WriteBlock(block)
+// 		} else {
+// 			nonce = nonce + 1
+// 			fmt.Println(nonce, hash)
+// 			// fmt.Println(block.Data + block.PreviousHash + strconv.FormatUint(uint64(block.Index), 10) + strconv.Itoa(nonce))
+// 		}
 
-	}
+// 	}
+// 	return errors.New("error"), false
+// }
+
+func (chain *Chain) CreateBlock(block *Block, data []byte) (error, Block) {
+	var b Block
+	b.PreviousHash = block.Hash
+	b.Data = data
+	return errors.New("error"), b
+}
+
+func (chain *Chain) GenerateHash(block *Block, nonce uint32) (error, []byte) {
+	o := [][]byte{block.Data, block.PreviousHash}
+	var n []byte
+	binary.LittleEndian.PutUint32(n, nonce)
+	j := bytes.Join(o, n)
+	sum := sha256.Sum256(j)
+	return errors.New("error"), sum[:]
+}
+
+func (chain *Chain) Genesis() error {
+	var b Block
+	b.Hash = []byte("0000000000000000000000000000000000000000000000000000000000000000")
+	b.Index = 0
+	chain.WriteBlock(&b)
 	return errors.New("error")
 }
 
 func main() {
 
 	var chain Chain
-	chain.LastHash = "1"
+	chain.LastHash = []byte{}
 	chain.Dificulty = 2
+	chain.Genesis()
 
-	for i := 0; i < 5; i++ {
+	// Create new chain
+	// Initialize genesis of chain
+	// Create a new block
+	// Run PoW
+	// Write block in chain
 
-		var b Block
-		b.Index = uint64(i)
-		b.Data = strconv.Itoa(i)
-		b.PreviousHash = chain.LastHash
-		chain.PoW(&b)
-		chain.LastHash = b.Hash
-		// sum := sha256.Sum256([]byte(b.Data + b.PreviousHash + strconv.FormatUint(uint64(b.Nonce), 10)))
-		// b.Hash = hex.EncodeToString(sum[:])
-		// fmt.Println(b.Hash)
-		// fmt.Println(b.Hash[:dificulty])
+	// for i := 0; i < 5; i++ {
 
-		// chain.WriteBlock(&b)
+	// 	var b Block
+	// 	b.Index = uint64(i)
+	// 	binary.LittleEndian.PutUint16(b.Data, uint16(i))
+	// 	b.PreviousHash = chain.LastHash
+	// 	chain.PoW(&b)
+	// 	chain.LastHash = b.Hash
+	// 	// sum := sha256.Sum256([]byte(b.Data + b.PreviousHash + strconv.FormatUint(uint64(b.Nonce), 10)))
+	// 	// b.Hash = hex.EncodeToString(sum[:])
+	// 	// fmt.Println(b.Hash)
+	// 	// fmt.Println(b.Hash[:dificulty])
 
-	}
+	// 	// chain.WriteBlock(&b)
 
-	// for _, n := range chain.Blocks {
-	// 	fmt.Println(n.Index, n.Data, n.Hash, n.PreviousHash)
 	// }
 
-	// fmt.Println(chain)
+	for _, n := range chain.Blocks {
+		fmt.Println(n.Index, n.Data, n.Hash, n.PreviousHash)
+	}
+
+	fmt.Println(chain)
 
 }
