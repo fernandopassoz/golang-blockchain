c44399b681787fecc47ed6dfdaa69022a80b20db
Fernando Passos <fernandopassoz@gmail.com>
2022-01-27 23:26:04 -0300

    generate hash and initial pow working

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,14 +1,17 @@
 package blockchain
 
 import (
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
 	"errors"
 	"fmt"
+	"time"
 )
 
 type Block struct {
-	Index        uint64
-	Nonce        int32
-	Data         []byte
+	Nonce        uint32
+	Data         []Transaction
 	PreviousHash string
 	Hash         string
 }
@@ -36,51 +39,71 @@ func (chain *Chain) WriteBlock(block *Block) error {
 	return errors.New("error")
 }
 
-// func (chain *Chain) PoW(block *Block) (error, bool){
-// 	golden := false
-// 	nonce := 0
-// 	sum := sha256.Sum256([]byte(block.Data + block.PreviousHash + strconv.FormatUint(uint64(block.Index), 10) + strconv.Itoa(nonce)))
-// 	var hash string = hex.EncodeToString(sum[:])
+func (chain *Chain) PoW(block *Block) *Block {
+	golden := false
+	nonce := 0
+	start := time.Now().UnixMilli()
 
-// 	for !golden {
-// 		// if string(block.PreviousHash)[:chain.Dificulty] == string(hash)[:chain.Dificulty] {
-// 		if "00" == string(hash)[:chain.Dificulty] {
-// 			golden = true
-// 			block.Nonce = int32(nonce)
-// 			block.Hash = hash
-// 			chain.WriteBlock(block)
-// 		} else {
-// 			nonce = nonce + 1
-// 			fmt.Println(nonce, hash)
-// 			// fmt.Println(block.Data + block.PreviousHash + strconv.FormatUint(uint64(block.Index), 10) + strconv.Itoa(nonce))
-// 		}
+	for !golden {
+		hash := chain.GenerateHash(block.Data, block.PreviousHash, uint32(nonce))
 
-//
-// 	return errors.New("error"), false
-// }
+		if string(block.PreviousHash)[:chain.Dificulty] == string(hash)[:chain.Dificulty] {
+			golden = true
+			block.Nonce = uint32(nonce)
+			block.Hash = hash
+			stop := time.Now().UnixMilli()
+			// chain.WriteBlock(block)
+			fmt.Println(nonce, hash, stop-start)
+		} else {
+			nonce++
+			// fmt.Println("wrong", nonce, hash)
+			// fmt.Println(block.Data + block.PreviousHash + strconv.FormatUint(uint64(block.Index), 10) + strconv.Itoa(nonce))
+		}
+	}
 
-// func (chain *Chain) CreateBlock(data []byte) (error, Block) {
-// 	var lastBlock = chain.Blocks[len(chain.Blocks) - 1]
-//   var b Block
-//   b.Data = data
-//   b.PreviousHash = lastBlock.Hash
-// 	return errors.New("error"), b
-// }
+	return block
+}
 
-// func (chain *Chain) GenerateHash(data []byte, previousHash string, nonce uint32) (error, []byte) {
-// 	o := [][]byte{data, []byte(previousHash)}
-// 	var n []byte
-// 	binary.LittleEndian.PutUint32(n, nonce)
-// 	j := bytes.Join(o, n)
-// 	sum := sha256.Sum256(j)
-// 	return errors.New("error"), sum[:]
-// }
+func (chain *Chain) CreateBlock(data []Transaction) Block {
+	lastBlock := chain.Blocks[len(chain.Blocks)-1]
+	var b Block
+	b.Data = data
+	b.PreviousHash = lastBlock.Hash
+	return b
+}
 
-// func (chain *Chain) Genesis() error {
-// 	var b Block
-// 	b.PreviousHash = "0000000000000000000000000000000000000000000000000000000000000000"
-// 	b.Index = 0
-//   b.Hash = chain.GenerateHash([]byte{}, b.PreviousHash, 0)
-// 	chain.WriteBlock(&b)
-// 	return errors.New("error")
-// }
+func (chain *Chain) GenerateHash(data []Transaction, previousHash string, nonce uint32) string {
+
+	// Nonce to byte - ok
+	n := make([]byte, 32)
+	binary.LittleEndian.PutUint32(n, nonce)
+
+	// Trasaction to byte - ok
+	var t []byte
+	for _, v := range data {
+		t = append(t, v.Data...)
+	}
+
+	// Previous hash to byte
+	p := []byte(previousHash)
+
+	// j := [][]byte{n, t, p}
+
+	// a := bytes.Join(j, []byte{})
+
+	var a []byte
+	a = append(a, n...)
+	a = append(a, t...)
+	a = append(a, p...)
+
+	sum := sha256.Sum256(a)
+	return hex.EncodeToString(sum[:])
+}
+
+func (chain *Chain) Genesis() error {
+	var b Block
+	b.PreviousHash = "0000000000000000000000000000000000000000000000000000000000000000"
+	b.Hash = chain.GenerateHash([]Transaction{}, b.PreviousHash, uint32(0))
+	chain.WriteBlock(&b)
+	return errors.New("error")
+}
--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"shiigo/blockchain"
 )
 
 func main() {
-	var n []byte
-	var nonce uint32
-	blockchain.Test()
-	binary.LittleEndian.PutUint32(n[0:], nonce)
-	fmt.Println(n)
+	// n := make([]byte, 32)
+	// var nonce uint32
+	// nonce = 123456
+	// blockchain.Test()
+	// // binary.LittleEndian.PutUint32(n[0:], nonce)
+	// binary.LittleEndian.PutUint32(n, nonce)
+	// // binary.Write(n, binary.LittleEndian, nonce)
+	// // binary.PutUvarint(n, uint64(nonce))
+	// fmt.Println(n)
+
+	var bc blockchain.Chain
+	var blk blockchain.Block
+	var ts blockchain.Transaction
+	var ts2 blockchain.Transaction
+	var tss []blockchain.Transaction
+
+	ts.Data = []byte("Hello World!")
+	ts2.Data = []byte("Another World!!!")
+	tss = append(tss, ts, ts2)
+
+	bc.Dificulty = 6
+
+	blk.Data = tss
+	blk.PreviousHash = "0000000000000000000000000000000000000000000000000000000000000000"
+
+	bc.PoW(&blk)
+
+	fmt.Println(blk)
 }
